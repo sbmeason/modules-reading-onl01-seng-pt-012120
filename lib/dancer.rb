1d43edae1b81d3a9954af0a230908bd9790d0703
Samantha Meason <sbmeason@gmail.com>
2020-02-24 02:09:02 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,8 +3,8 @@ require_relative './dance_module.rb'
 class Dancer
   include Dance
   extend MetaDancing
-  include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   
   attr_accessor :name
   
