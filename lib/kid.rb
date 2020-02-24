33c55490541fbbf1758c54995b4a855021d413b1
Samantha Meason <sbmeason@gmail.com>
2020-02-24 02:09:34 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,6 @@ require_relative './class_methods_module.rb'
 class Kid
   include Dance
   extend MetaDancing
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
   
   attr_accessor :name
   def initialize(name)
