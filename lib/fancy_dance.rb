438f3115d34d7063cd4b3584171092bee651648a
Samantha Meason <sbmeason@gmail.com>
2020-02-24 01:35:04 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -17,4 +17,9 @@ module FancyDance
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
+  
+  def metadata
+      "This class produces objects that love to dance."
+    end
+  end
 end
